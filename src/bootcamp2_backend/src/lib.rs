3dcdb7ca4aa11fa3adb39e23aa2d5c0d0fea5dd6
JoojoCoding <weichergames345@gmail.com>
2024-06-25 18:55:57 +0200

    wpisy

--- src/bootcamp2_backend/src/lib.rs
+++ src/bootcamp2_backend/src/lib.rs
@@ -1,4 +1,16 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn greet(name: String, lastname: String, age: i8) -> String {
+    format!("Hello, {} {} {}!", name, lastname, age)
+}
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String){
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        wpisy.borrow_mut().push(wpis)
+    });
 }
--- src/bootcamp2_frontend/src/App.jsx
+++ src/bootcamp2_frontend/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    bootcamp2_backend.greet(name).then((greeting) => {
+    const lastname = event.target.elements.lastname.value;
+    const age = event.target.elements.age.value;
+    bootcamp2_backend.greet(name, lastname, parseInt(age)).then((greeting) => {
       setGreeting(greeting);
     });
     return false;
@@ -21,6 +23,10 @@ function App() {
       <form action="#" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
+        <label htmlFor="lastname">Enter your name: &nbsp;</label>
+        <input id="lastname" alt="LastName" type="text" />
+        <label htmlFor="age">Enter your name: &nbsp;</label>
+        <input id="age" alt="Age" type="text" />
         <button type="submit">Click Me!</button>
       </form>
       <section id="greeting">{greeting}</section>
